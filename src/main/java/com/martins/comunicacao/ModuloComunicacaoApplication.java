c1ff714ab8c5135f69a85d3c418917ac333b1e10
Valdelar <valdelar.martins@gmail.com>
2020-11-30 22:46:20 -0300

    commit inicial com estrutura do projeto

---
+++ src/main/java/com/martins/comunicacao/ModuloComunicacaoApplication.java
@@ -0,0 +1,13 @@
+package com.martins.comunicacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuloComunicacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModuloComunicacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/martins/comunicacao/ModuloComunicacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.martins.comunicacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuloComunicacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
